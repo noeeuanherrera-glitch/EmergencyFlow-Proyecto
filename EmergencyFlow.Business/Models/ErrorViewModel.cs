e09dbdd3c441ba1e973b5cdc5e642e78a5ce8f82
Noe Euan Herrera <noe.euan@tecdesoftware.edu.mx>
2026-05-16 05:06:15 -0600

    Agregar archivos de proyecto.

---
+++ EmergencyFlow.Business/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmergencyFlow.Business.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
